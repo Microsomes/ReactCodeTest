b08038fde4c0d067aab6526b6bb6dcf29f5c75e3
Tayyab Javed <tayyab.javed@grv.media>
2023-05-31 22:01:55 +0100

    boiler plate coded

---
+++ src/app/components/DogSelector.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import react,{useState,useEffect} from 'react';
+
+
+
+export default function DogSelector() {
+
+    return (
+        <div>
+            <h1>dogSelector</h1>
+        </div>
+    )
+
+}
